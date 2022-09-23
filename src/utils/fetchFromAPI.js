82eddd7fbb707fa9ee4b5a5e29f901eca4a7f9cb
Bhargav Chaotra <78082418+BhargavDelta@users.noreply.github.com>
2022-09-24 02:32:13 +0530

    Add files via upload

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '1a892cadabmshedeb479661e8acdp1951e5jsn661c49b47ad9',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
